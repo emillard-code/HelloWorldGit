d97f6d18a8f69ffb81bbd7ce6b196a31a23d1d51
Emerson Millard <emillard.code@gmail.com>
2024-06-24 12:00:00 -0700

    Minor formatting corrections.

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -2,9 +2,9 @@ package com.project;
 
 public class Main {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
 
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
 
     }
 
